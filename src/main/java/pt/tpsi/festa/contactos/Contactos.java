8baa68ef7d98bebfe8196b34d6cbcbadcda77d1f
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-05-10 15:45:49 +0100

    first commit
    testing creating 4 classes to contacts

---
+++ src/main/java/pt/tpsi/festa/contactos/Contactos.java
@@ -0,0 +1,15 @@
+package pt.tpsi.festa.contactos;
+
+public class Contactos {
+
+	// 1 - Atributos
+
+	// 2 - Construtores
+
+	// 3 - Getters And Setters
+
+	// 4 - Comportamentos
+
+	// 5 - Métodos Complementares
+
+}
---
+++ src/main/java/pt/tpsi/festa/contactos/ContactosEmpresariais.java
@@ -0,0 +1,13 @@
+package pt.tpsi.festa.contactos;
+
+public class ContactosEmpresariais extends Contactos {
+	// 1 - Atributos
+
+	// 2 - Construtores
+
+	// 3 - Getters And Setters
+
+	// 4 - Comportamentos
+
+	// 5 - Métodos Complementares
+}
