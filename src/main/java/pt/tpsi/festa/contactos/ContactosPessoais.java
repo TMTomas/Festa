79b859b46c2676fd955b317fc023fae265910860
Francisco <franciscosimoes2002@hotmail.com>
2023-05-10 16:15:36 +0100

    ContactosPessoais class

--- src/main/java/pt/tpsi/festa/contactos/ContactosPessoais.java
+++ src/main/java/pt/tpsi/festa/contactos/ContactosPessoais.java
@@ -1,13 +1,34 @@
 package pt.tpsi.festa.contactos;
 
+import java.util.Date;
+
 public class ContactosPessoais extends Contactos {
+	
 	// 1 - Atributos
-
+	
+	protected Date dataNascimento;
+	
 	// 2 - Construtores
+	
+	//CONTRUTOR DEFAULT
+	public ContactosPessoais() {
+	}
+	
+	// CONTRUTOR COM PARAMETROS
+	public ContactosPessoais(String primeiroNome, String ultimoNome, String numero, Date dataNascimento) {
+		super(primeiroNome, ultimoNome, numero);
+		this.dataNascimento = dataNascimento;
+	}
 
+	
 	// 3 - Getters And Setters
-
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	
 	// 4 - Comportamentos
-
+	
+	
 	// 5 - Métodos Complementares
+	
 }
