bdd406ce7483323c5e2b9100742828c0e7fef168
Tiago <>
2023-05-16 18:48:32 +0100

    Adicionar versão rudimentar da interface para a classe espaço (wip)

---
+++ src/main/java/pt/tpsi/festa/espaco/EspacoInterface.java
@@ -0,0 +1,19 @@
+package pt.tpsi.festa.espaco;
+
+public interface EspacoInterface {
+
+	public static void importar(/* base de dados (WIP)*/) {
+		
+	}
+	
+	public static void selecionar(int index) {
+		
+	}
+	
+	public static void consultar(int index) {
+		
+	}
+	
+	// Extremamente simples confirmar comportamentos com o grupo do espaço ASAP
+	
+}
