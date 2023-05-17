547f6dab049becae771aa9114fc29c82aeb80970
Tiago <>
2023-05-17 15:52:42 +0100

    adicioar interface

---
+++ src/main/java/pt/tpsi/festa/comesebebes/CeBInterface.java
@@ -0,0 +1,21 @@
+package pt.tpsi.festa.comesebebes;
+
+public interface CeBInterface {
+
+		
+	public static void consultar(/* mapper de produtos, quantidades e preços */) {
+		
+	}
+	
+	public static void adicionar(int produto, int quantidade) {
+		//adicionar a outro mapper de quantidades
+	}
+	
+	public static void alterar(int quantidade) {
+		
+	}
+	public static void remover(int index) {
+		
+	}
+	
+}
