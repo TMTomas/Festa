7feb7149fcaec4ba8fbe50df062930994810219c
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2023-05-17 14:56:20 +0100

    Add method to convert temperature to celcius

--- src/main/java/pt/tpsi/festa/Main.java
+++ src/main/java/pt/tpsi/festa/Main.java
@@ -6,6 +6,8 @@ import pt.brunojesus.locationsearch.openstreetmap.model.OpenStreetMapLocation;
 import pt.tpsi.festa.espaco.http.MetereologiaRequest;
 import pt.tpsi.festa.espaco.model.MetereologiaModel;
 
+import java.util.List;
+
 /**
  * Classe principal que demonstra o uso da classe MetereologiaRequest e
  * MetereologiaModel.
@@ -23,12 +25,26 @@ public class Main {
 		 // Cria uma instância de MetereologiaRequest
 		MetereologiaRequest request = new MetereologiaRequest();
 		OpenStreetMap openStreetMap = new OpenStreetMap();
-		OpenStreetMapLocation openStreetMapLocation = new OpenStreetMapLocation();
-		openStreetMap.search("CNEMA, Santarém");
-		// Cria uma solicitação de metereologia
-		MetereologiaModel model = request.createMetrologiaRequest( openStreetMapLocation.getLatitute(),openStreetMapLocation.getLongitude());
 
-		System.out.println(model.getTemperatura().temp);
+		List<OpenStreetMapLocation> locations =  openStreetMap.search("CNEMA, Santarem");
+
+		MetereologiaModel model = request.createMetrologiaRequest(locations.get(0).getLatitute(), locations.get(0).getLongitude());
+
+		System.out.println(model.getTemperatura().getTempC());
+
+		/*
+		locations.forEach(location -> {
+			System.out.println("Latitude: " + location.getLatitute());
+			System.out.println("Longitude: " + location.getLongitude());
+			// Cria uma solicitação de metereologia
+			MetereologiaModel model = request.createMetrologiaRequest( location.getLatitute(),location.getLongitude());
+
+			System.out.println(model);
+		});
+
+		 */
+
+
 
 
 	}
--- src/main/java/pt/tpsi/festa/espaco/model/Main.java
+++ src/main/java/pt/tpsi/festa/espaco/model/Main.java
@@ -4,12 +4,22 @@ import lombok.Data;
 
 @Data
 public class Main {
-    public double temp;
-    public double feels_like;
-    public double temp_min;
-    public double temp_max;
-    public int pressure;
-    public int humidity;
-    public int sea_level;
-    public int grnd_level;
+    private double temp;
+    private double feels_like;
+    private double temp_min;
+    private double temp_max;
+    private int pressure;
+    private int humidity;
+    private int sea_level;
+    private int grnd_level;
+
+    public int getTempC(){
+        return (int) (temp - 273);
+    }
+    public int getMinTempC(){
+        return (int) (temp_min - 273);
+    }
+    public int getMaxTempC(){
+        return (int) (temp_max - 273);
+    }
 }
