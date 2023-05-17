cda7fcc967d3cced6aa14be491ec285e324932d8
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-05-17 17:07:38 +0100

    .

--- src/main/java/pt/tpsi/festa/contactos/model/ConvidadosList.java
+++ src/main/java/pt/tpsi/festa/contactos/model/ConvidadosList.java
@@ -3,7 +3,7 @@ package pt.tpsi.festa.contactos.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConvidadosList extends Contact implements ContactInterface{
+public class ConvidadosList implements ContactInterface {
 
 	// Atributos
 	protected List<Contact> convidados;
@@ -32,6 +32,34 @@ public class ConvidadosList extends Contact implements ContactInterface{
 	}
 
 	// Comportamentos
+
+	@Override
+	public void listarContactos(ArrayList<Contact> contactos) {
+		for (Contact contato : contactos) {
+			return (contato.getFirstName() + " " + contato.getLastName());
+		}
+	}
+
+	@Override
+	public void listarConvidados(ArrayList<Convidados> convidados) {
+		for (ConvidadosList convidado : convidados) {
+			return (convidado.getFirstName() + " " + convidado.getLastName());
+		}
+	}
+
+	@Override
+	public void convidar(int index) {
+		Contact contato = getConvidados().add(index);
+		// Lógica para convidar o contato
+		// ...
+	}
+
+	@Override
+	public void desconvidar(int index) {
+		Contact contato = getConvidados().remove(index);
+		// Lógica para desconvidar o contato
+		// ...
+	}
 
 	// public void convidar(Contact convidado) {
 	// convidados.add(convidado);
