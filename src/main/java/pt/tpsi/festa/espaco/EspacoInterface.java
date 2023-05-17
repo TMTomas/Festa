f3aec2af31f5d5ceab85841ae21cd387055d8836
Tiago <>
2023-05-17 15:35:07 +0100

    retificar interface

--- src/main/java/pt/tpsi/festa/espaco/EspacoInterface.java
+++ src/main/java/pt/tpsi/festa/espaco/EspacoInterface.java
@@ -2,18 +2,11 @@ package pt.tpsi.festa.espaco;
 
 public interface EspacoInterface {
 
-	public static void importar(/* base de dados (WIP)*/) {
-		
-	}
-	
 	public static void selecionar(int index) {
 		
 	}
 	
-	public static void consultar(int index) {
+	public static void consultar(/* mapper=?????????????? */) {
 		
-	}
-	
-	// Extremamente simples confirmar comportamentos com o grupo do espaço ASAP
-	
+	}	
 }
