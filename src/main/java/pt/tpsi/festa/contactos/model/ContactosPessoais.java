68cbd647a1edac43f334b051557696d2c824550a
Francisco <franciscosimoes2002@hotmail.com>
2023-05-17 14:42:54 +0100

    Class Contact eliminada, modificações na classe ContactosPessoais

--- src/main/java/pt/tpsi/festa/contactos/model/ContactosPessoais.java
+++ src/main/java/pt/tpsi/festa/contactos/model/ContactosPessoais.java
@@ -1,15 +1,25 @@
 package pt.tpsi.festa.contactos.model;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-import pt.brunojesus.contactslib.ContactApi;
+import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor;
+
 import pt.brunojesus.contactslib.model.Contact;
 
-public class ContactosPessoais extends pt.tpsi.festa.contactos.model.Contact {
+/**
+ * 
+ * 
+ * @author Francisco Simões
+ * 
+ */
+public class ContactosPessoais extends Contact{
 	
 	// 1 - Atributos
-	ArrayList<ContactApi> contactosPessoais;
+	protected List<Contact> contactosPessoais;
 	protected Date dataDeNascimento;
 	
 	// 2 - Construtores
@@ -19,18 +29,34 @@ public class ContactosPessoais extends pt.tpsi.festa.contactos.model.Contact {
 	}
 	
 	// CONTRUTOR COM PARAMETROS
-	public ContactosPessoais(String firstName, String lastName, Date dataDeNascimento) {
-	super();
+	public ContactosPessoais(Date dataDeNascimento, List<ContactosPessoais> contactosPessoais) {
 	this.dataDeNascimento = dataDeNascimento;
+	this.contactosPessoais = new ArrayList<Contact>();
 	}
 
-	
 	// 3 - Getters And Setters
+	public List<Contact> getContactosPessoais() {
+		return contactosPessoais;
+	}
 
+	public void setContactosPessoais(List<Contact> contactosPessoais) {
+		this.contactosPessoais = contactosPessoais;
+	}
+
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
 	
 	// 4 - Comportamentos
 	
-	
+//	public void saveToFile(String filename) throws IOException, FileNotFoundException {}
+//	
+//	public void loadToFile(String filename) throws IOException, FileNotFoundException {}
+
 	// 5 - Métodos Complementares
 	
 }
