a7ab174436966e66e33a829564953ecd51249839
Tiago <>
2023-05-17 16:02:06 +0100

    corrigir syntax

--- src/main/java/pt/tpsi/festa/contactos/model/ContactosInterface.java
+++ src/main/java/pt/tpsi/festa/contactos/model/ContactosInterface.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 
 public interface ContactosInterface {
 	
-	public static void listarContactos(ArrayList<API> Contactos) {
+	public void listarContactos(ArrayList<Contactos> Contactos);
 		// lista os contactos
-	}
 	
-	public static void listarConvidados(ArrayList<Convidados> Convidados){
+	public void listarConvidados(ArrayList<Convidados> Convidados);
 		// lista e adiciona a um novo arraylist os contactos convidados
-	}
-	public static void convidar(int index) {
-		// convida apartir indice do arraylist
-	}
 	
-	public static void desconvidar(int index) {
+	public void convidar(int index);
+		// convida apartir indice do arraylist
+	
+	public void desconvidar(int index);
 		// desconvida apartir indice do arraylist
-
-	}
 	
 }
