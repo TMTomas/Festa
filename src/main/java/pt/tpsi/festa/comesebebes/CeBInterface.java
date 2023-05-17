83bf6eafa916f56a62d19d2be5a46139286db3ce
Tiago <>
2023-05-17 15:57:59 +0100

    correcoes

--- src/main/java/pt/tpsi/festa/comesebebes/CeBInterface.java
+++ src/main/java/pt/tpsi/festa/comesebebes/CeBInterface.java
@@ -1,21 +1,9 @@
 package pt.tpsi.festa.comesebebes;
 
 public interface CeBInterface {
-
 		
-	public static void consultar(/* mapper de produtos, quantidades e preços */) {
-		
-	}
-	
-	public static void adicionar(int produto, int quantidade) {
-		//adicionar a outro mapper de quantidades
-	}
-	
-	public static void alterar(int quantidade) {
-		
-	}
-	public static void remover(int index) {
-		
-	}
-	
+	public void consultar(/* mapper de produtos, quantidades e preços */);
+	public void adicionar(int produto, int quantidade);
+	public void alterar(int quantidade); 
+	public void remover(int index);	
 }
