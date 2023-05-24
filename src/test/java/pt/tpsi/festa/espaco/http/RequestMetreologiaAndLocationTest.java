ad56d5d151dabba63de4f0886da3ab88b40ccc1f
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2023-05-24 20:58:45 +0100

    finalize Junit Tests

--- src/test/java/pt/tpsi/festa/espaco/http/RequestMetreologiaAndLocationTest.java
+++ src/test/java/pt/tpsi/festa/espaco/http/RequestMetreologiaAndLocationTest.java
@@ -1,37 +1,47 @@
 package pt.tpsi.festa.espaco.http;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import pt.tpsi.festa.espaco.model.Location;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RequestMetreologiaAndLocationTest {
-    // TODO Fazer os testes
-    //Criar um mockito para listas
-    private final RequestMetreologiaAndLocation request = new RequestMetreologiaAndLocation();
-    @BeforeAll
+    private static RequestMetreologiaAndLocation request;
+    List<Location> list;
+    @BeforeEach
     void setUp() {
-        List<Location> list = new ArrayList<>();
+        // String latitude, String longitude, String weather
+        list = List.of(new Location("CMAS,Santarem", "1000", "20000", "cloudy"));
+
     }
 
     @Test
     void pesquisar(){
-        request.pesquisar("CNEMA, Santarem");
+       request = Mockito.mock(RequestMetreologiaAndLocation.class);
+       Mockito.when(request.pesquisar("CMAS,Santarem")).thenReturn(list);
+
+       List<Location> result = request.pesquisar("CMAS,Santarem");
+       assertEquals(list.get(0),result.get(0));
     }
     @Test
     void selecionar() {
-        for(int i = 0; i < request.getLocationList().size(); i++){
-            assertEquals(request, request.selecionar(i));
-        }
+        request = Mockito.mock(RequestMetreologiaAndLocation.class);
+        Mockito.when(request.selecionar(0)).thenReturn(list.get(0));
+
+        assertEquals(list.get(0),request.selecionar(0));
     }
 
     @Test
     void selecionarPorNome() {
-        assertEquals("cnema",request.selecionarPorNome("Cnema"));
+        request = Mockito.mock(RequestMetreologiaAndLocation.class);
+        Mockito.when(request.selecionarPorNome("CMAS")).thenReturn(list.get(0));
+
+        Location result = request.selecionarPorNome("CMAS");
+        assertEquals(list.get(0),result);
     }
 
 
