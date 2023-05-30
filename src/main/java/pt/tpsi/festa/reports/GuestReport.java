22b34220429eceb81ec174604e2e4e7b88926843
Tiago <>
2023-05-30 22:19:25 +0100

    create GuestReport class

---
+++ src/main/java/pt/tpsi/festa/reports/GuestReport.java
@@ -0,0 +1,25 @@
+package pt.tpsi.festa.reports;
+
+import pt.brunojesus.report.common.ReportData;
+import pt.brunojesus.report.guestlist.model.GuestReportData;
+import pt.tpsi.festa.contactos.model.ConvidadosList;
+
+public class GuestReport {
+
+	// 1 ATRIBUTOS
+	
+    ReportData reportData = new GuestReportData();
+    ConvidadosList listaConvidados = new ConvidadosList();
+
+	// 2 ACESSORES
+	
+	// 3 CONSTRUTORES
+	
+	// 4 COMPORTAMENTOS
+	
+     
+    
+	// 5 METODOS COMPLEMENTARES
+	
+	
+}
