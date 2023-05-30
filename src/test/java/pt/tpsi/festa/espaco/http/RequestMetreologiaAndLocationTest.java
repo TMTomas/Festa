6edcf21e118de6c7218012e5d8bb0496569a140c
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2023-05-31 00:51:29 +0100

    Solve problems in espaço

--- src/test/java/pt/tpsi/festa/espaco/http/RequestMetreologiaAndLocationTest.java
+++ src/test/java/pt/tpsi/festa/espaco/http/RequestMetreologiaAndLocationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import pt.tpsi.festa.espaco.model.Location;
+import pt.tpsi.festa.espaco.model.LocationPlus;
 
 import java.util.List;
 
@@ -11,37 +12,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RequestMetreologiaAndLocationTest {
     private static RequestMetreologiaAndLocation request;
-    List<Location> list;
+    List<Location> listBase;
+    List<LocationPlus> listPlus;
     @BeforeEach
     void setUp() {
         // String latitude, String longitude, String weather
-        list = List.of(new Location("CMAS,Santarem", "1000", "20000", "cloudy"));
-
+        listBase = List.of(new Location("CMAS,Santarem", "1000", "20000", "cloudy"));
+        listPlus = List.of(new LocationPlus("CMas,Santarem", "1000", "20000", "cloudy", 10,
+        5, 10, "administrativo", " "));
     }
 
     @Test
-    void pesquisar(){
+    void pesquisarListBase(){
        request = Mockito.mock(RequestMetreologiaAndLocation.class);
-       Mockito.when(request.pesquisar("CMAS,Santarem")).thenReturn(list);
+        request.pesquisar("CMAS,Santarem");
+       Mockito.when(request.getLocationListBase()).thenReturn(listBase);
 
-       List<Location> result = request.pesquisar("CMAS,Santarem");
-       assertEquals(list.get(0),result.get(0));
+       List<Location> result = request.getLocationListBase();
+       assertEquals(listBase.get(0),result.get(0));
+    }
+    @Test
+    void pesquisarListPlus(){
+        request = Mockito.mock(RequestMetreologiaAndLocation.class);
+        request.pesquisar("CMAS,Santarem");
+        Mockito.when(request.getLocationListPlus()).thenReturn(listPlus);
+
+        List<Location> result = request.getLocationListBase();
+        assertEquals(listPlus.get(0),result.get(0));
     }
     @Test
     void selecionar() {
         request = Mockito.mock(RequestMetreologiaAndLocation.class);
-        Mockito.when(request.selecionar(0)).thenReturn(list.get(0));
+        Mockito.when(request.selecionar(0)).thenReturn(listPlus.get(0));
 
-        assertEquals(list.get(0),request.selecionar(0));
+        assertEquals(listPlus.get(0),request.selecionar(0));
     }
 
     @Test
     void selecionarPorNome() {
         request = Mockito.mock(RequestMetreologiaAndLocation.class);
-        Mockito.when(request.selecionarPorNome("CMAS")).thenReturn(list.get(0));
+        Mockito.when(request.selecionar("CMAS")).thenReturn(listPlus.get(0));
 
-        Location result = request.selecionarPorNome("CMAS");
-        assertEquals(list.get(0),result);
+        Location result = request.selecionar("CMAS");
+        assertEquals(listPlus.get(0),result);
     }
 
 
