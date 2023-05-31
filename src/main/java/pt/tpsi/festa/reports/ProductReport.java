ef6a5301a28f8c4d1825e1bd3ac18de8fca451a9
Tiago <>
2023-05-31 22:13:57 +0100

    Add javadoc

--- src/main/java/pt/tpsi/festa/reports/ProductReport.java
+++ src/main/java/pt/tpsi/festa/reports/ProductReport.java
@@ -1,3 +1,18 @@
+/**
+ * 
+ * @author António Ferraz
+ * @author Eduardo Ribeiro
+ * @author Nuno Dias
+ * @author Tiago Tomás
+ * 
+ * Menções importantes
+ * @author ChatGPT (Mais valia de alguns membros)
+ * @author Daniel Duarte (Obrigado por toda a ajuda e disponibilidade)
+ * 
+ * Esta classe cria um ProductReport, que se pode exportar para Html, Pdf e Xml.
+ * É também possível visualizar o Report sem se criar um ficheiro. Para mais informações consultar a superclasse {@link ReportExporter.java}.
+ *
+ */
 package pt.tpsi.festa.reports;
 
 import java.util.ArrayList;
@@ -9,7 +24,7 @@ import pt.brunojesus.report.common.ReportCompiler;
 import pt.brunojesus.report.common.ReportData;
 import pt.brunojesus.report.productlist.model.Product;
 import pt.brunojesus.report.productlist.model.ProductReportData;
-
+import pt.tpsi.festa.comesebebes.model.Carrinho;
 import pt.tpsi.festa.comesebebes.model.ComesEBebes;
 
 public class ProductReport extends ReportExporter{
@@ -55,7 +70,13 @@ public class ProductReport extends ReportExporter{
 	}
 	// COMPORTAMENTOS
 	
-	public List<Product> compileCarrinho() {
+	/**
+	 * Método que "compila" a lista de compras, resume-se em usar a "List<Product> list" e preenchê-la com a ListaDeCompras da classe {@link Carrinho.java} para ser poder usada no productReportData.
+	 * 
+	 * @return list
+	 */
+	
+	public List<Product> compileShoppingList() {
 
 		for (int i = 0; i < comesEBebes.getCarrinho().getListaDeCompras().size(); i++) {
 			list.add(new Product().setId(String.valueOf(i))
@@ -66,7 +87,14 @@ public class ProductReport extends ReportExporter{
 		return list;
 
 	}
-	
+	/**
+	 * Atribui valores ao reportData
+	 *  
+	 * @param partyName	Nome da festa
+	 * @param date	Data da festa (MM/DD/AAAA HH:mm)
+	 * @param address Morada da festa
+	 * @return 
+	 */
 	public ReportData productReportData(String partyName, String date, String address) {
 		 reportData = new ProductReportData()
                 .setName(partyName)
@@ -79,7 +107,7 @@ public class ProductReport extends ReportExporter{
 	}
 	
 	// METODOS COMPLEMENTARES
-
+	
 	public boolean equals(ProductReport productReport) {
 
 		if (productReport == productReport)
