4df6714989cbcb1f67a38f3c31932032f82297a6
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-05-31 14:43:52 +0100

    alteracao da api para a classe convidados list
    alteraçao da interface para suportar as alteraçoes da classe ConvidadosList
    
    Main alterada para nao conter a ContactAPI

--- src/main/java/pt/tpsi/festa/Main.java
+++ src/main/java/pt/tpsi/festa/Main.java
@@ -2,70 +2,38 @@ package pt.tpsi.festa;
 
 import java.util.List;
 
-import pt.brunojesus.contactslib.ContactApi;
-import pt.brunojesus.contactslib.model.Contact;
 import pt.tpsi.festa.contactos.model.ConvidadosList;
-import pt.tpsi.festa.reports.ProductReport;
-
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		// Instanciar a classe ConvidadosList
+		ConvidadosList convidadosList = new ConvidadosList();
 
-		System.out.println("IntStream");
-		System.out.println("Has " + Runtime.getRuntime().availableProcessors() + " processors");
+		// Listar os contactos disponíveis
+		System.out.println("Contactos Disponíveis:");
+		for (String contacto : convidadosList.listarContactos()) {
+			System.out.println(contacto);
+		}
 
-		final ContactApi contactApi = new ContactApi();
+		// Convidar um contacto
+		convidadosList.convidar(0, convidadosList.getListaCompleta(), "teste");
 
-		long start = System.currentTimeMillis();
-		List<Contact> l1 = contactApi.generateContacts(4);
-		long finish = System.currentTimeMillis();
-		long timeElapsed = finish - start;
+		// Listar os convidados
+		System.out.println("\nConvidados:");
+		for (String convidado : convidadosList.listarConvidados()) {
+			System.out.println(convidado);
+		}
 
-		ProductReport productReport = new ProductReport();
-		
-		productReport.getComesEBebes().adicionar("batata frita", 0, 7, false);
-		productReport.getComesEBebes().adicionar("morangos", 0, 5, false);
-		productReport.getComesEBebes().adicionar("cornetto", 0, 5, false);
-		productReport.getComesEBebes().adicionar("pastilha gorila", 0, 5, false);
-		productReport.getComesEBebes().adicionar("nuggets", 0, 3, false);
+		// Desconvidar um contacto
+		convidadosList.desconvidar(0);
 
-		productReport.createPDF();
-
-		
-
-//		  ReportData guestReportData = new GuestReportData()
-//	                .setEventName("My event name")
-//	                .setEventDate(new Date())
-//	                .setAddress("ESGTS\nIPSantarem")
-//	                .setGuests(
-//	                        List.of(
-//	                                new Guest().setName("Bruno Jesus").setContact("+351911234567").setPresent(true),
-//	                                new Guest().setName("Guest 1").setContact("+351911234568").setPresent(false),
-//	                                new Guest().setName("Guest 2").setContact("student@ipsantarem.pt")
-//	                        )
-//	 
-//		  Report guestReport = compiler.apply(guestReportData);
-//		  
-
-//		  viewer.accept(guestReport);
+		// Listar os convidados novamente
+		System.out.println("\nConvidados:");
+		for (String convidado : convidadosList.listarConvidados()) {
+			System.out.println(convidado);
+		}
 	}
 
-
-		ConvidadosList listaConvidados = new ConvidadosList();
-
-		listaConvidados.convidar(0, l1, "nao gosta de vodka");
-		listaConvidados.convidar(1, l1, "nao bebe alcool");
-
-		System.out.println("LISTA COMPLETA DE CONTACTOS:");
-		System.out.println(listaConvidados.listarContactos(l1));
-		System.out.println();
-
-		System.out.println("LISTA DOS CONVIDADOS:");
-		System.out.println(listaConvidados.listarConvidados());
-		System.out.println();
-
-		System.out.printf("Generating %s contacts to invite took %sms\n", l1.size(), timeElapsed);
-	}
 }
