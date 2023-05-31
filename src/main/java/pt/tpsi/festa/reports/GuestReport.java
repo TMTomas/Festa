8fa83c73b62899abc6c7ffdc226dc6905e8b70e6
Tiago <>
2023-05-31 22:51:00 +0100

    Add GuestReport

--- src/main/java/pt/tpsi/festa/reports/GuestReport.java
+++ src/main/java/pt/tpsi/festa/reports/GuestReport.java
@@ -1,58 +1,94 @@
 package pt.tpsi.festa.reports;
 
+import pt.brunojesus.contactslib.model.Contact;
 import pt.brunojesus.report.common.ReportData;
+import pt.brunojesus.report.guestlist.model.Guest;
 import pt.brunojesus.report.guestlist.model.GuestReportData;
+import pt.brunojesus.report.productlist.model.Product;
 import pt.tpsi.festa.contactos.model.ConvidadosList;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class GuestReport extends ReportExporter{
 
 	// 1 ATRIBUTOS
 
-	ConvidadosList convidadosList;
+	ConvidadosList convidados;
+	protected List<Guest> list;
 	
 	// 2 ACESSORES
 	
-	public ConvidadosList getConvidadosList() {
-		return convidadosList;
+	public ConvidadosList getConvidados() {
+		return convidados;
+	}
+	
+	public List<Guest> getList() {
+		return list;
 	}
 	
 	// 3 CONSTRUTORES
-	
+
 	public GuestReport() {
 		super();
-		convidadosList = new ConvidadosList();
+		convidados = new ConvidadosList();
+		list =  new ArrayList<>();
 	}
 	
-    public GuestReport(ConvidadosList convidadosList) {
+    public GuestReport(ConvidadosList convidadosList, List<Guest> list) {
 		super();
-		this.convidadosList = convidadosList;
+		this.convidados = convidadosList;
+		this.list = list;
 	}
     
     public GuestReport(GuestReport guestReport) {
-		super();
-    	guestReport.getConvidadosList();
+    	this(guestReport.getConvidados(), guestReport.getList());
     }
-
+    
 	// 4 COMPORTAMENTOS
+    
+    public List<Guest> compileShoppingList() {
+
+		for (int i = 0; i < convidados.getListaConvidados().size(); i++) {
+			
+			list.add(new Guest().setName(convidados.getListaConvidados().get(i).getFirstName() + " " + convidados.getListaConvidados().get(i).getLastName())
+					.setContact(convidados.getListaConvidados().get(i).getPhoneNumber())
+					.setPresent(true));
+				
+			}
+		
+		return list;
+
+	}
 	
 	public ReportData productReportData(String partyName, String date, String address) {
 		 return reportData = new GuestReportData()
                .setEventName(partyName)
                .setEventDate(new Date(date))
                .setAddress(address)
-               .setGuests(null);
+               .setGuests(list);
 		
 	}
-
-	
 	
 	// 5 METODOS COMPLEMENTARES
 	
+	public boolean equals(GuestReport guestReport) {
+
+		if (guestReport == guestReport)
+			return false;
+		else
+			return list.equals(guestReport.getConvidados());
+	}
+
+	public GuestReport clone() {
+
+		return new GuestReport(this);
+	}
+
 	@Override
 	public String toString() {
-		return "GuestReport [convidadosList=" + convidadosList + "]";
+		return "GuestReport [convidadosList=" + convidados + "]";
 	}
 	
 }
